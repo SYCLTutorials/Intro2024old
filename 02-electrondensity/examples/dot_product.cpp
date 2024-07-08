76231de4351b711cd882b7ba9ae6d94b51b1b4f6
Jorge Velez <jorgevmg31@gmail.com>
2024-07-08 15:07:34 -0500

    Updated README and add basic examples.

---
+++ 02-electrondensity/examples/dot_product.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <sycl/sycl.hpp>
+
+class vector_dot_product;
+
+int main() {
+    const int N = 1024; // Length of the vectors
+    std::vector<int> vectorA(N, 1); // Vector A filled with 1s
+    std::vector<int> vectorB(N, 2); // Vector B filled with 2s
+    int result = 0; // Result of dot product initialized to zero
+
+    auto gpu_selector = sycl::gpu_selector{};
+    try {
+        auto gpuQueue = sycl::queue{gpu_selector};
+
+        {
+            auto bufA = sycl::buffer{vectorA.data(), sycl::range{N}};
+            auto bufB = sycl::buffer{vectorB.data(), sycl::range{N}};
+            auto bufResult = sycl::buffer{&result, sycl::range{1}};
+
+            gpuQueue.submit([&](sycl::handler& cgh) {
+                auto accA = bufA.get_access<sycl::access::mode::read>(cgh);
+                auto accB = bufB.get_access<sycl::access::mode::read>(cgh);
+                auto accResult = bufResult.get_access<sycl::access::mode::read_write>(cgh);
+
+                cgh.parallel_for<vector_dot_product>(sycl::range{N}, [=](sycl::id<1> idx) {
+                    int i = idx[0];
+                    accResult[0] += accA[i] * accB[i];
+                });
+            }).wait();
+
+            std::cout << "Dot product completed successfully on GPU. Result: " << result << '\n';
+        }
+
+    } catch (sycl::exception const& e) {
+        std::cerr << "No GPU device found. Error: " << e.what() << '\n';
+        std::cerr << "Trying to fallback to CPU.\n";
+        auto cpuQueue = sycl::queue{sycl::cpu_selector{}};
+
+        {
+            auto bufA = sycl::buffer{vectorA.data(), sycl::range{N}};
+            auto bufB = sycl::buffer{vectorB.data(), sycl::range{N}};
+            auto bufResult = sycl::buffer{&result, sycl::range{1}};
+
+            cpuQueue.submit([&](sycl::handler& cgh) {
+                auto accA = bufA.get_access<sycl::access::mode::read>(cgh);
+                auto accB = bufB.get_access<sycl::access::mode::read>(cgh);
+                auto accResult = bufResult.get_access<sycl::access::mode::read_write>(cgh);
+
+                cgh.parallel_for<vector_dot_product>(sycl::range{N}, [=](sycl::id<1> idx) {
+                    int i = idx[0];
+                    accResult[0] += accA[i] * accB[i];
+                });
+            }).wait();
+
+            std::cout << "Dot product completed on CPU. Result: " << result << '\n';
+        }
+    }
+
+    return 0;
+}
