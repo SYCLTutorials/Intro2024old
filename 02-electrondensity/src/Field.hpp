84d2d4f676f27e808216b3f69bfca3e55dbcd343
RayHE <rayhe88@gmail.com>
2024-08-29 15:11:19 -0500

    Update Field.hppp delete config

--- 02-electrondensity/src/Field.hpp
+++ 02-electrondensity/src/Field.hpp
@@ -94,25 +94,6 @@ public:
   }
 
 
-  void configGrid(int  *npx, int *npy, int *npz, double *delta,
-                  double *xmin, double *ymin, double *zmin,
-                  size_t *nsize){
-
-     (*xmin) = -10.0;
-     (*ymin) = -10.0;
-     (*zmin) = -10.0;
-
-     (*delta) = 0.25;
-
-     (*npx) = fabs(2.*(*xmin)) / (*delta);
-     (*npy) = fabs(2.*(*ymin)) / (*delta);
-     (*npz) = fabs(2.*(*zmin)) / (*delta);
-
-     (*nsize) = (*npx) * (*npy) * (*npz);
-
-   }
-
-
 private:
   Wavefunction &wf;
   double xmin, ymin, zmin;
